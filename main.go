aaa82312934807299f341dca9d38e106ce2ffe33
muidea <rangh@foxmail.com>
2018-02-22 19:50:37 +0800

    update sourceCode

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"flag"
-	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"io/ioutil"
@@ -18,6 +18,8 @@ import (
 
 var (
 	dpi        = flag.Float64("dpi", 72, "screen resolution in Dots Per Inch")
+	backcolor  = flag.Uint("backcolor", 0xFFFF0000, "backcolor for avatar")
+	fontcolor  = flag.Uint("fontcolor", 0xFF0000FF, "fontcolor for avatar")
 	fontfile   = flag.String("fontfile", "./font.ttf", "filename of the ttf font")
 	avatarfile = flag.String("avatarfile", "./avatar.png", "filename of the avatar file")
 	text       = flag.String("text", "", "the avatar content")
@@ -25,6 +27,16 @@ var (
 	width      = flag.Int("width", 64, "avatar width")
 	hight      = flag.Int("hight", 64, "avatar hight")
 )
+
+func rgbaColor(val uint32) color.RGBA {
+	ret := color.RGBA{}
+	ret.R = uint8(val >> 24)
+	ret.G = uint8(val >> 16 & 0x00FF)
+	ret.B = uint8(val >> 8 & 0x00FF)
+	ret.A = uint8(val & 0x00FF)
+
+	return ret
+}
 
 func main() {
 	flag.Parse()
@@ -47,9 +59,12 @@ func main() {
 	}
 
 	// Initialize the context.
-	fg, bg := image.Black, image.White
+	frontGround, backGround := image.Black, image.White
+	frontGround = image.NewUniform(rgbaColor(uint32(*fontcolor)))
+	backGround = image.NewUniform(rgbaColor(uint32(*backcolor)))
+
 	rgba := image.NewRGBA(image.Rect(0, 0, *width, *hight))
-	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+	draw.Draw(rgba, rgba.Bounds(), backGround, image.ZP, draw.Src)
 
 	fontContext := freetype.NewContext()
 	fontContext.SetDPI(*dpi)
@@ -57,7 +72,7 @@ func main() {
 	fontContext.SetFontSize(*size)
 	fontContext.SetClip(rgba.Bounds())
 	fontContext.SetDst(rgba)
-	fontContext.SetSrc(fg)
+	fontContext.SetSrc(frontGround)
 	fontContext.SetHinting(font.HintingFull)
 
 	// Draw the text.
@@ -70,7 +85,6 @@ func main() {
 		yOffset = (*hight+fontSize)/2 - fontSize/8
 	}
 
-	log.Printf("avatar=%s,fontSize=%d, xOffset=%d, yOffset=%d", string(avatarVal), fontSize, xOffset, yOffset)
 	pt := freetype.Pt(xOffset, yOffset)
 	_, err = fontContext.DrawString(string(avatarVal), pt)
 	if err != nil {
@@ -85,16 +99,15 @@ func main() {
 		os.Exit(1)
 	}
 	defer outFile.Close()
-	b := bufio.NewWriter(outFile)
-	err = png.Encode(b, rgba)
+	bufferWriter := bufio.NewWriter(outFile)
+	err = png.Encode(bufferWriter, rgba)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
-	err = b.Flush()
+	err = bufferWriter.Flush()
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println("Wrote out.png OK.")
 }
